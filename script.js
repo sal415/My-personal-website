1726964dd7c0fe6434f0e66090de652424de42a5
SalmanDoria <salmandoria@gmail.com>
2022-10-24 16:41:58 -0400

    add script.js and style.css,connect to index.html

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const h2 = document.createElement("h2")
-h2.textContent = "This content added by Javascript";
-document.querySelector("body").appendChild(h2);
+const nycbutton = document.querySelector("#nyc")
+nycbutton.addEventListener('click',function(){
+    console.log ("I have been clicked")
+   const imagecontainer = document.querySelector("#image-container")
+   const nycimg = document.createElement("img")
+   nycimg.src="https://www.travelandleisure.com/thmb/91pb8LbDAUwUN_11wATYjx5oF8Q=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/new-york-city-evening-NYCTG0221-52492d6ccab44f328a1c89f41ac02aea.jpg"
+    imagecontainer.appendChild(nycimg)
+})
